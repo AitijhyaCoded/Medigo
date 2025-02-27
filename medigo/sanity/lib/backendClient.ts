44c5e5f13afb261eaf3a33f865b645235527ec0a
Rudranil Das <rudranild04@gmail.com>
2025-02-27 13:27:30 +0530

    Made changes in Backend

--- medigo/sanity/lib/backendClient.ts
+++ medigo/sanity/lib/backendClient.ts
@@ -7,9 +7,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  stega: {
-    studioUrl: process.env.NEXT_PUBLIC_STUDIO_URL
-    ? `https://${process.env.NEXT_PUBLIC_STUDIO_URL}/studio`
-    : '${process.env.NEXT_PUBLIC_VERCEL_URL}/studio',
-  }
+  token: process.env.SANITY_API_TOKEN,
 })
